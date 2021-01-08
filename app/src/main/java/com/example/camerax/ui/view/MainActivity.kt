67ac5468ab4a86b5819bbb0253aa074c4bed13a2
vmehra25 <vmehra25@yahoo.com>
2021-01-08 23:41:47 +0530

    Camera Working

--- app/src/main/java/com/example/camerax/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/camerax/ui/view/MainActivity.kt
@@ -3,8 +3,15 @@ package com.example.camerax.ui.view
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Rational
+import android.util.Size
+import android.view.ViewGroup
+import androidx.camera.core.CameraX
+import androidx.camera.core.Preview
+import androidx.camera.core.PreviewConfig
 import androidx.core.app.ActivityCompat
 import com.example.camerax.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,11 +20,31 @@ class MainActivity : AppCompatActivity() {
 
 
         if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
-
+            txv.post{
+                startCamera()
+            }
         }else{
             ActivityCompat.requestPermissions(this,
             arrayOf(android.Manifest.permission.CAMERA),
             1234)
         }
+    }
+
+    private fun startCamera() {
+        val previewConfig = PreviewConfig.Builder().apply {
+            setTargetResolution(Size(1080, 1080))
+            setTargetAspectRatio(Rational(1, 1))
+        }.build()
+
+        val preview = Preview(previewConfig)
+        preview.setOnPreviewOutputUpdateListener {
+            val parent = txv.parent as ViewGroup
+            parent.removeView(txv)
+            parent.addView(txv, 0)
+            txv.setSurfaceTexture(it.surfaceTexture)
+        }
+
+        CameraX.bindToLifecycle(this, preview)
+
     }
 }
